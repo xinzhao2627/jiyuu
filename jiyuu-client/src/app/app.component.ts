51aab78db812ced8454bcd3056a1bdc053bbb23f
Rainnand <xinzhao2627@gmail.com>
2025-05-04 23:45:42 +0800

    init 2

--- jiyuu-client/src/app/app.component.ts
+++ jiyuu-client/src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Buffer } from 'buffer';
+
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'client';
+  title = 'jiyuu-client';
 
-  send() {
-    console.log('sending!!');
-
-    let ss = 'send this to my extension!!';
-
-    // TODO: send ss as message to extension
+  sendMsg() {
+    console.log('jelly');
   }
 }
